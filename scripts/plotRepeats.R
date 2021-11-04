e22da09cfa52a3cbe6f2380141eaf5684143433c
Caspar Gross <mail@caspar.one>
2021-11-04 13:52:24 +0100

    Added sequence text to waterfall plots

--- scripts/plotRepeats.R
+++ scripts/plotRepeats.R
@@ -33,8 +33,6 @@ suppressMessages(library(tidyverse))
 suppressMessages(library(dendextend))
 
 dir.create(dirname(out_p))
-
-
 plot_repeat_lengths <- function(dt) {
 
 # Calculate plot limits for nice and uniform display
@@ -48,7 +46,6 @@ if ((xmax - xmin) < WMIN) {
     xmax <- xmax + floor(delta/2)
 }
 
-print(c(xmin, xmax))
 # Plot repeat length histogram
 title = dt$locus_id[[1]]
 p_rl <- ggplot(dt, aes(x = repeat_length, fill = as.factor(allele))) +
@@ -65,16 +62,16 @@ ggsave(paste(out_p,dt$locus_id[[1]],"repeat_lengths",ext, sep="."), plot = p_rl)
 return(dt)
 } 
 
-plot_waterfall <- function(dts) {
+plot_waterfall <- function(dt) {
 # Create waterfall plot
-dts <- dts %>% 
+dts <- dt %>% 
     arrange(desc(repeat_start_genomic + repeat_length)) %>%
     mutate(seq_name = factor(seq_name, unique(seq_name))) %>%
-    mutate(repeat_matches = str_locate_all(repeat_sequence, consensus_sequence)) %>%
+    mutate(repeat_matches = str_locate_all(repeat_sequence, as.character(consensus_sequence))) %>%
     unnest(repeat_matches) %>%
     mutate(x = ((repeat_matches[,1] + repeat_matches[,2])/2) + repeat_start_genomic) %>%
     rowwise() %>%
-    mutate(w = nchar(consensus_sequence)) 
+    mutate(w = nchar(as.character(consensus_sequence)))
 
 # Create a minimum number of blank lines in plot
 min_plot_lines <- 30
@@ -83,12 +80,32 @@ n_blank <- max(0, min_plot_lines - length(seqs))
 blank <- paste0("blank_" , as.character(1:n_blank))
 seqs <- c(seqs, blank)
 
+# Add sequence chars to the plot
+dtt <- dt %>% 
+    mutate(repeat_sequence = strsplit(repeat_sequence, split="")) %>%
+    unnest(repeat_sequence) %>%
+    group_by(seq_name) %>%
+    mutate(x = row_number() + repeat_start_genomic) %>%
+    summarise(x, repeat_sequence, allele)  %>% as.data.frame()
+
+dt %>% 
+    mutate(repeat_sequence = strsplit(repeat_sequence, split="")) %>%
+    unnest(repeat_sequence) %>%
+    group_by(seq_name) %>%
+    mutate(x = row_number() + repeat_start_genomic) %>%
+    summarise(x, repeat_sequence, allele) %>%
+    str()
+
 p_wf <- ggplot() +
-    geom_blank(aes(y = seqs)) +
-    geom_tile(data = dts, aes(
+    #geom_blank(aes(y = seqs)) +
+    geom_text(data = dtt, aes(
             y = seq_name,
-            x= x,
-            width = w,
+            x=x,
+            label = repeat_sequence)) +
+    geom_tile(data= dts, aes(
+            y = seq_name,
+            x=x,
+            width = w,  
             fill = consensus_sequence,
             height=0.9 
             ),
@@ -96,6 +113,9 @@ p_wf <- ggplot() +
         size = 0.2) +
     theme_bw() +
     ggtitle(dts$locus_id[[1]]) + 
+#   scale_fill_brewer(palette = "Set3", limits = levels(dt_phased$consensus_sequence)) +
+    scale_fill_brewer(palette = "Set3") +
+    facet_grid(rows = vars(allele), space = "free_y", scales = "free_y") +
     theme(
         axis.text.y = element_blank(), 
         axis.ticks.y = element_blank(),
@@ -108,9 +128,9 @@ ggsave(paste(out_p,dts$locus_id[[1]],"waterfall",ext, sep="."), width = 15, plot
 return(dts)
 }
 
-dt_phased <- read_csv(repeats, col_types = "fcfnccnnfcc")
+dt_phased <- read_csv(repeats, col_types = "fcfnfcnnfcc")
 
 dt_phased %>%
     group_by(locus) %>%
-    group_modify(~plot_repeat_lengths(.x)) %>%
+#    group_modify(~plot_repeat_lengths(.x)) %>%
     group_modify(~plot_waterfall(.x))
